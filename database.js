24670d1e46c374e7aeda26a065e56cc86fae301e
Michael Straughan <mstraughan86@gmail.com>
2017-07-22 20:00:00 -0700

    It would help if I could easily start up MongoDB and use a GUI in-tandem

---
+++ database.js
@@ -0,0 +1,10 @@
+const mongoose = require('./mongoose.js');
+
+// This was to initialize MongoDB in a separate process in order to facilitate
+// proper database functionality.
+// This appears to be deprecated, as proper shutdown timing avoids this need.
+const main = () => {
+  mongoose.initialize();
+};
+
+main();
