ef89d4f7771cfd521e148250580fbb1ba34ad35f
Michael Straughan <mstraughan86@gmail.com>
2017-07-23 20:00:00 -0700

    Database script cleanup

--- database.js
+++ database.js
@@ -1,10 +1,4 @@
 const mongoose = require('./mongoose.js');
 
-// This was to initialize MongoDB in a separate process in order to facilitate
-// proper database functionality.
-// This appears to be deprecated, as proper shutdown timing avoids this need.
-const main = () => {
-  mongoose.initialize();
-};
-
+const main = () => mongoose.initialize();
 main();
