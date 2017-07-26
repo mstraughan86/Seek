07a54e20e57dd7c523afed78d9840652bcadc5da
Michael Straughan <mstraughan86@gmail.com>
2017-07-25 20:00:00 -0700

    Mongoose helper functions cleanup

--- mongoose.js
+++ mongoose.js
@@ -12,30 +12,17 @@ const mongoDB = new mongod({
   dbpath: path.join(__dirname, '.', 'mongodb', 'data')
 });
 
-const videoSchema = new mongoose.Schema({
-  title: String,
-  url: String,
-  aliasUrl: String,
-  number: Number,
-  season: Number,
-  episode: Number,
-  description: String,
-  video: String,
-  trailer: String,
-  imageThumb: String
-});
-const Video = mongoose.model("Video", videoSchema);
-
 const followerSchema = new mongoose.Schema({
   login: String
 });
 const FollowerToProcess = mongoose.model("FollowerToProcess", followerSchema);
 const ProcessedFollower = mongoose.model("ProcessedFollower", followerSchema);
+const Temporary = mongoose.model("Temporary", followerSchema);
 
 const schemas = {
-  Video : Video,
   FollowerToProcess : FollowerToProcess,
-  ProcessedFollower : ProcessedFollower
+  ProcessedFollower : ProcessedFollower,
+  Temporary : Temporary
 };
 
 const initialize = () => {
@@ -62,32 +49,13 @@ const disconnect = () => {
   return mongoose.disconnect(()=>{console.log('Mongoose: Disconnected.')});
 };
 const save = (Model, dataArray) => {
-	//console.log(dataArray);
   return new Promise((resolve, reject)=>{
     Model.create(dataArray, function (err, results) {
       if (err) return reject(err);
-      //console.log('Mongoose: Save successful!');
-
-      // saved!
-      resolve();
+      return resolve(dataArray);
     })
   });
-
-  // return Model.insertMany(dataArray).then((docs) => {
-  //   console.log('SAVED!');
-  //   console.log(docs);
-  // })
-
-/*
-
- https://www.google.com/search?q=how+to+backup+mongodb&rlz=1C1GGRV_enUS751US751&oq=how+to+backup+mongo&aqs=chrome.0.0j69i57j0l4.4128j0j7&sourceid=chrome&ie=UTF-8
- https://docs.mongodb.com/manual/core/backups/
- https://docs.mongodb.com/manual/tutorial/backup-and-restore-tools/
-
-
- */
 };
-
 
 module.exports = {
   connect,
